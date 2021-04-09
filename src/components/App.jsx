9f1e5578380a7922d3ac00b7ab9e921c27dc24f4
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-09 18:20:49 +0300

    Split content.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,5 @@
-import FoodData from './FoodData';
+import Content from './Content';
 
 export default function App() {
-  return (
-    <div
-      style={{
-        textAlign: 'center',
-        background: 'black',
-        minHeight: '100vh',
-      }}>
-      <div style={{ paddingTop: '20vh' }}>
-        <div style={{ fontSize: '5vh', fontStyle: 'italic', color: 'gray' }}>*Loud italian noises*</div>
-        <div style={{ fontSize: '7vh', fontWeight: 'bold', color: 'red' }}>&quot;EY PIZZA MAMA&quot;</div>
-        <div style={{ fontSize: '5vh', fontStyle: 'italic', color: 'gray' }}>ETC...</div>
-        <div>
-          <ul>
-            {FoodData.map((category, index) => (
-              <div key={index}>
-                <div style={{ fontSize: '3vh', fontWeight: 'bold', fontStyle: 'italic', color: 'green' }}>
-                  {category.categoryName}
-                </div>
-                <ul style={{ listStyle: 'none' }}>
-                  {category.data.map((data, index) => (
-                    <li key={index}>{JSON.stringify(data)}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+  return <Content />;
 }
