ecf5b4e7f4c8b325ccb892b5be8247075924414e
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-10 18:37:29 +0300

    Added theme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { createMuiTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
 import Layout from './components/Layout';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        background: '#556779',
+      },
+    },
+  },
+  palette: {
+    background: {
+      default: '#111A23',
+    },
+    primary: {
+      main: '#2A4158',
+    },
+    secondary: {
+      main: '#556779',
+    },
+    text: {
+      primary: '#AAB3BC',
+      secondary: '#AAB3BC',
+    },
+  },
+});
+
 export default function App() {
-  return <Layout />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout />
+    </ThemeProvider>
+  );
 }
