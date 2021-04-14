1d443e519a786724bf0829e751710bed7fc6744e
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-14 14:21:02 +0300

    Added background image to products container.

--- src/components/products/ProductsContainer.jsx
+++ src/components/products/ProductsContainer.jsx
@@ -2,11 +2,14 @@ import { makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import NavBarEmptyHeightFill from '../nav-bar/NavBarEmptyHeightFill';
 
+import BackgroundImage from '../../images/productsBackground.jpg';
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(3),
 
+    backgroundImage: `url('${BackgroundImage}')`,
     background: theme.palette.text.secondary,
 
     minHeight: '100vh',
