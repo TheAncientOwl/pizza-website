5addcc6742adae1396b19dfc67a6bcad892c8862
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-15 15:55:50 +0300

    added className.

--- src/components/products/product/ProductSummary.jsx
+++ src/components/products/product/ProductSummary.jsx
@@ -1,14 +1,15 @@
 import { Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-export default function ProductSummary({ summary }) {
+export default function ProductSummary({ className, summary }) {
   return (
-    <Typography variant='body1' color='textPrimary'>
+    <Typography className={className} variant='body1' color='textPrimary'>
       {summary}
     </Typography>
   );
 }
 
 ProductSummary.propTypes = {
+  className: PropTypes.any,
   summary: PropTypes.string.isRequired,
 };
